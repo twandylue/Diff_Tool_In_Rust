0e1be0e38437e26c2aa5b9ba0f82a2cfefa4a41b
AndyLu <laserlue@gmail.com>
2022-11-02 20:34:53 +0800

    compare letters

--- diff_tool/src/lib.rs
+++ diff_tool/src/lib.rs
@@ -1,5 +1,54 @@
 use ::std::cmp;
 
+pub struct Diff {
+    addition: Vec<char>,
+    removal: Vec<char>,
+    unchanged: Vec<char>,
+}
+
+impl Diff {
+    pub fn new() -> Self {
+        Diff {
+            addition: Vec::new(),
+            removal: Vec::new(),
+            unchanged: Vec::new(),
+        }
+    }
+}
+
+pub fn diff(text1: &Vec<char>, text2: &Vec<char>) -> Diff {
+    let lcs = compute_lcs_len_dp(text1, text2);
+    let mut i = text1.len();
+    let mut j = text2.len();
+
+    let mut result = Diff::new();
+
+    while i != 0 || j != 0 {
+        if i == 0 {
+            result.addition.push(text2[j - 1]);
+            j -= 1;
+        } else if j == 0 {
+            result.removal.push(text1[i - 1]);
+            i -= 1;
+        } else if text1[i - 1] == text2[j - 1] {
+            result.unchanged.push(text1[i - 1]);
+            i -= 1;
+            j -= 1;
+        } else if lcs[j - 1][i] <= lcs[j][i - 1] {
+            result.addition.push(text1[i - 1]);
+            i -= 1;
+        } else {
+            result.removal.push(text2[j - 1]);
+            j -= 1;
+        }
+    }
+
+    result.addition.reverse();
+    result.removal.reverse();
+    result.unchanged.reverse();
+    return result;
+}
+
 pub fn compute_lcs_string_dp(text1: &Vec<char>, text2: &Vec<char>) -> String {
     let mut result = String::new();
     let mut i = text1.len();
@@ -84,5 +133,16 @@ mod tests {
         let r = compute_lcs_string_dp(&s1, &s2);
 
         assert_eq!(r, "abcd");
+    }
+
+    #[test]
+    fn diff_test() {
+        let s1: Vec<char> = String::from("abcdefghi").chars().collect();
+        let s2: Vec<char> = String::from("azedbcz").chars().collect();
+        let r = diff(&s1, &s2);
+
+        assert_eq!(r.addition.iter().collect::<String>(), "defghi");
+        assert_eq!(r.unchanged.iter().collect::<String>(), "abc");
+        assert_eq!(r.removal.iter().collect::<String>(), "zedz");
     }
 }
