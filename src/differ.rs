c33ad9206c877ef917bced183136ff82ec2b06c7
AndyLu <laserlue@gmail.com>
2023-03-17 23:29:49 +0800

    feat: add compute_lev
    Use The Levenshtein Algorithm.

--- src/differ.rs
+++ src/differ.rs
@@ -30,6 +30,77 @@ impl Differ {
         return dp;
     }
 
+    fn compute_lev(text1: String, text2: String) -> Vec<(char, usize, char)> {
+        let c1 = text1.chars().collect::<Vec<char>>();
+        let c2 = text2.chars().collect::<Vec<char>>();
+        let n = text1.len();
+        let m = text2.len();
+        let mut tables = vec![vec![0; m + 1]; n + 1];
+        tables[0][0] = 0;
+        let mut actions = vec![vec!['-'; m + 1]; n + 1];
+        actions[0][0] = 'I';
+
+        for i in 1..n + 1 {
+            actions[i][0] = 'R';
+            tables[i][0] = i;
+        }
+
+        for j in 1..m + 1 {
+            actions[0][j] = 'A';
+            tables[0][j] = j;
+        }
+
+        for i in 1..n + 1 {
+            for j in 1..m + 1 {
+                if c1[i - 1] == c2[j - 1] {
+                    actions[i][j] = 'I';
+                    tables[i][j] = tables[i - 1][j - 1];
+                    continue;
+                }
+
+                let remove = tables[i - 1][j];
+                let add = tables[i][j - 1];
+
+                tables[i][j] = remove;
+                actions[i][j] = 'R';
+
+                if add < tables[i][j] {
+                    tables[i][j] = add;
+                    actions[i][j] = 'A';
+                }
+
+                tables[i][j] += 1;
+            }
+        }
+
+        // trace_cache(&tables, &actions, &text1, &text2);
+
+        let mut patch: Vec<(char, usize, char)> = Vec::new();
+        let mut n1 = n;
+        let mut n2 = m;
+        while n1 > 0 || n2 > 0 {
+            let action = &actions[n1][n2];
+            if *action == 'A' {
+                n2 -= 1;
+                patch.push(('A', n2, c2[n2]));
+            } else if *action == 'R' {
+                n1 -= 1;
+                patch.push(('R', n1, c1[n1]));
+            } else if *action == 'I' {
+                n1 -= 1;
+                n2 -= 1;
+            } else {
+                unreachable!("can't be!");
+            }
+        }
+
+        for (action, pos, c) in &patch {
+            println!("{action}, {pos}, {c}");
+        }
+
+        return patch;
+    }
+
     fn compute_lcs(old_text: &Vec<String>, new_text: &Vec<String>) -> Vec<String> {
         let lcs = Self::compute_lcs_matrix_dp(new_text, old_text);
         let mut i = new_text.len();
@@ -90,6 +161,30 @@ impl Differ {
             .collect::<Vec<String>>();
 
         return Self::compute_lcs(&old_text, &new_text);
+    }
+}
+
+fn trace_cache(tables: &Vec<Vec<usize>>, actions: &Vec<Vec<char>>, text1: &str, text2: &str) {
+    let c2 = text2.chars().collect::<Vec<char>>();
+    let c1 = text1.chars().collect::<Vec<char>>();
+    let mut s = String::new();
+    for c in c2 {
+        s.push_str(&format!("{c}    "));
+    }
+    println!("        {s}");
+
+    for row in 0..tables.len() {
+        for col in 0..tables[row].len() {
+            if row != 0 && col == 0 {
+                print!("{c}", c = c1[row - 1]);
+            }
+            print!(
+                " {value}({action})",
+                value = tables[row][col],
+                action = actions[row][col]
+            );
+        }
+        println!();
     }
 }
 
@@ -244,5 +339,20 @@ mod tests {
         for i in 0..actual.len() {
             assert_eq!(actual[i], expected[i]);
         }
+    }
+
+    #[test]
+    fn compute_lev_ok() {
+        // arrange
+        let text1 = "add".to_string();
+        // let text2 = "daddy".to_string();
+        let text2 = "adyd".to_string();
+        let expected = vec![('A', 2, 'y')];
+
+        // act
+        let actual = Differ::compute_lev(text1, text2);
+
+        // assert
+        assert_eq!(expected, actual);
     }
 }
